a79a95851bb4f181b6fe62730ad2272e756278b5
michael240893 <michael.schimpelsberger@aon.at>
2021-05-13 12:43:15 +0200

    cors added

--- app/main.py
+++ app/main.py
@@ -14,11 +14,14 @@ from sklearn import preprocessing
 from sklearn import metrics
 import csv
 import math
+from flask_cors import CORS
 
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
 app = Flask(__name__)
-  
+
+CORS(app)
+
 # The route() function of the Flask class is a decorator, 
 # which tells the application which URL should call 
 # the associated function.
